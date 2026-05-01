65087805ba08712231f5bea598ff66b52c3943ad
SharmaVaibhav976531 <vaibhavsharma976531@gmail.com>
2026-05-01 19:43:14 +0530

    Update Installed Apps in settings.py file

--- lms_core/settings.py
+++ lms_core/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Custom Apps
+    'apps.accounts',
+    'apps.library',
+    'apps.transactions',
+    'apps.reports',
 ]
 
 MIDDLEWARE = [
